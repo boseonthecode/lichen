dd568a376a227ee759f455419f602d5c05a6c681
Aryaroop Bose <224219618+boseonthecode@users.noreply.github.com>
2026-03-28 02:34:19 +0530

    Add backward function to Tensor class

--- tensor.py
+++ tensor.py
@@ -26,15 +26,20 @@ class Add(Function):
         return x+y
     
     def backward(self, grad):
-        return grad,grad
+        x,y = self.tensors[0],self.tensors[1]
+        grad_x = grad if x.requires_grad else None
+        grad_y = grad if y.requires_grad else None
+        return grad_x,grad_y
     
 class Mul(Function):
     def forward(self, x, y):
         return x*y
     
     def backward(self, grad):
-        x,y = self.tensors[0].data, self.tensors[1].data
-        return grad*y, grad*x
+        x,y = self.tensors[0], self.tensors[1]
+        grad_x = grad * y.data if x.requires_grad else None
+        grad_y = grad * x.data if y.requires_grad else None
+        return grad_x, grad_y
 
 class Tensor:
     def __init__(self, data, requires_grad=False):
@@ -69,3 +74,38 @@ class Tensor:
     def __rmul__(self, other):
         return self.__mul__(other)
     
+    def backward(self, grad=None):
+        if grad is None:
+            grad = np.ones_like(self.data)
+        self.grad=grad
+
+        order = []
+        visited = set()
+        stack = [(self,False)]
+
+        while(stack):
+            node, children_done = stack.pop()
+            if node in visited:
+                continue
+            if not children_done:
+                stack.append((node,True))
+                if node.creator is not None:
+                    for parent in node.creator.tensors:
+                        if parent.requires_grad:
+                            stack.append((parent,False))
+            else:
+                visited.add(node)
+                order.append(node)
+
+        for tensor in reversed(order):
+            if tensor.creator is None:
+                continue
+
+            grads = tensor.creator.backward(tensor.grad)
+            for inp, g in zip(tensor.creator.tensors, grads):
+                if g is None or not inp.requires_grad:
+                    continue
+                if inp.grad is None:
+                    inp.grad = g.copy()
+                else:
+                    inp.grad += g
