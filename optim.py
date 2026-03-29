24fdaecdf5405352c8626d019bdcd7e682b53cef
Aryaroop Bose <224219618+boseonthecode@users.noreply.github.com>
2026-03-29 18:02:14 +0530

    Add optimizer through optim.py

---
+++ optim.py
@@ -0,0 +1,13 @@
+class SGD:
+    def __init__(self, parameters, lr=0.01):
+        self.parameters = parameters
+        self.lr = lr
+
+    def zero_grad(self):
+        for p in self.parameters:
+            p.grad = None
+
+    def step(self):
+        for p in self.parameters:
+            if p.grad is not None:
+                p.data -= lr*p.grad
