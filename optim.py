3706a26d3a6c1b61ac08011581db8039fd8344f6
Aryaroop Bose <224219618+boseonthecode@users.noreply.github.com>
2026-03-29 20:42:42 +0530

    Add Adam optimizer

--- optim.py
+++ optim.py
@@ -1,3 +1,4 @@
+import numpy as np
 class SGD:
     def __init__(self, parameters, lr=0.01):
         self.parameters = parameters
@@ -11,3 +12,32 @@ class SGD:
         for p in self.parameters:
             if p.grad is not None:
                 p.data -= self.lr*p.grad
+
+class Adam:
+    def __init__(self, parameters, lr=0.001, beta1=0.9, beta2=0.999, eps=1e-8):
+        self.parameters = parameters
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+
+        self.t = 0
+
+        self.m = [np.zeros_like(p.data) for p in parameters]
+        self.v = [np.zeros_like(p.data) for p in parameters]
+
+    def zero_grad(self):
+        for p in self.parameters:
+            p.grad = None
+
+    def step(self):
+        self.t+=1
+        for i,p in enumerate(self.parameters):
+            if p.grad is not None:
+                g = p.grad
+                self.m[i] = self.beta1*self.m[i] + (1-self.beta1)*g
+                self.v[i] = self.beta2*self.v[i] + (1-self.beta2)*(g**2)
+                m_hat = self.m[i] / (1-self.beta1**self.t)
+                v_hat = self.v[i] / (1-self.beta2**self.t)
+
+                p.data -= self.lr*m_hat / (np.sqrt(v_hat)+self.eps)
