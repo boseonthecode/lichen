d5b45a61ab3732c0d1f44681f7cd4751c1bb95be
Aryaroop Bose <224219618+boseonthecode@users.noreply.github.com>
2026-04-07 09:43:44 +0530

    Add softmax and cross-entropy functions

--- functional.py
+++ functional.py
@@ -28,6 +28,33 @@ class _Sigmoid(Function):
     def backward(self, grad):
         return (grad*self.out*(1-self.out),)
     
+class _Softmax(Function):
+    def forward(self, x):
+        e = np.exp(x - x.max(axis=-1, keepdims=True))
+        self.out = e/e.sum(axis=-1, keepdims=True)
+        return self.out
+    
+    def backward(self, grad):
+        s = self.out
+        dot = (grad*s).sum(axis=-1, keepdims=True)
+        return(s*(grad-dot),)
+    
+class _CrossEntropyLoss(Function):
+    def forward(self, logits, targets):
+        batch = logits.shape[0]
+        e = np.exp(logits - logits.max(axis=1, keepdims = True))
+        self.probs = e/e.sum(axis=1, keepdims=True)
+        self.targets = targets.astype(int)
+        correct_probs = self.probs[np.arrange(batch), self.targets]
+        return np.array(-np.mean(np.log(correct_probs + 1e-9)))  
+    
+    def backward(self, grad):
+        batch = self.probs.shape[0]
+        d = self.probs.copy()
+        d[np.arrange(batch), self.targets] -=1
+        d = grad*d/batch
+        return d, None
+    
 def relu(x):
     return _Relu.apply(x)
 
@@ -36,3 +63,9 @@ def mse_loss(pred, target):
 
 def sigmoid(x):
     return _Sigmoid.apply(x)
+
+def softmax(x):
+    return _Softmax.apply(x)
+
+def cross_entropy(logits, targets):
+    return _CrossEntropyLoss.apply(logits, targets)
